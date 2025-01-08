c0bb6c0cb99341cbbd59ba3b78f1b3368bf02a00
Dmitry <dakimenko@yahoo.com>
2025-01-08 17:48:59 +0300

    Page connexion ajouté, erreurs affichés, token récuperé

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -79,3 +79,63 @@
                     }
             }
             
+
+            // Fonctions sign in //
+
+            const buttonSeConnecter = document.querySelector("#buttonSeConnecterId")
+            buttonSeConnecter.addEventListener("click", async() => {
+                event.preventDefault()
+
+                const champEmail = document.getElementById("email")
+                const champMotdePasse = document.getElementById("champMotDePasse")
+
+                if (verifierChamps(champEmail, champMotdePasse)) {
+                    const loginRequest = {"email": champEmail.value, "password": champMotdePasse.value}
+                    const loginRequestBody = JSON.stringify(loginRequest)
+                    const response = await fetch ("http://localhost:5678/api/users/login", {
+                        method: "POST",
+                        headers: {
+                            "accept": "application/json",
+                            "Content-Type": "application/json"
+                        },
+                        body: loginRequestBody
+                    })
+                    const returnValue = await response.json()
+                    if (returnValue.token === undefined) {
+                        loginFail()
+                    } else {
+                        const token = returnValue.token
+                        window.localStorage.setItem("token", token)
+                        loginSuccessful()
+                    }
+                }
+            })
+
+            const verifierChamps = (champEmail, champMotDePasse) => { 
+                if (champEmail.value === "" || champMotDePasse.value === "") {
+                    const errorMessageVide = document.createElement("p")
+                    errorMessageVide.innerText = "Ce champ est obligatoire"
+                    if (champEmail.value === "") {
+                        const errorArea = document.getElementById("emailDiv")
+                        errorArea.appendChild(errorMessageVide)
+                        return false
+                    } else {
+                        const errorArea = document.getElementById("motDePasseDiv")
+                        errorArea.appendChild(errorMessageVide)
+                        return false
+                    }
+                } else {return true}
+            }
+
+            const loginFail = () => {
+                const failedLogin = document.createElement("p")
+                failedLogin.innerText = "Email ou Mot de passe incorrect"
+                const errorArea = document.getElementById("motDePasseDiv")
+                errorArea.appendChild(failedLogin)
+            }
+
+            const loginSuccessful = () => {
+                window.location.href = "index.html"
+                console.log(window.localStorage.getItem("token"))
+            }
+
